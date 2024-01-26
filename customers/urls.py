e7b8cdf359bcb22216f857a617f822a629ea370c
Diego Castro <diego.castro@impellertech.com>
2024-01-26 15:53:18 -0500

    chore: added tests for customers service

--- customers/urls.py
+++ customers/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.CustomersView.as_view()),
-    path("<int:pk>/balance/", views.CustomerBalanceView.as_view()),
+    path("", views.CustomersView.as_view(), name="customers"),
+    path(
+        "<int:pk>/balance/",
+        views.CustomerBalanceView.as_view(),
+        name="customer-balance",
+    ),
 ]
