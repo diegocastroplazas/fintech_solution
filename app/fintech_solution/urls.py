858be99f37b4e95e5a0365f3e32ad305944151b3
Diego Castro <diego.castro@impellertech.com>
2024-01-28 21:59:48 -0500

    chore: path for docs

--- app/fintech_solution/urls.py
+++ app/fintech_solution/urls.py
@@ -4,26 +4,20 @@ from django.urls import include
 from rest_framework.authtoken.views import obtain_auth_token
 from drf_spectacular.views import (
     SpectacularAPIView,
-    SpectacularRedocView,
     SpectacularSwaggerView,
 )
 
 
 urlpatterns = [
+    path(
+        "",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
     path("admin/", admin.site.urls),
     path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
     path("api/customers/", include("customers.urls")),
     path("api/loans/", include("loans.urls")),
     path("api/payments/", include("payments.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path(
-        "api/schema/swagger-ui/",
-        SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger-ui",
-    ),
-    path(
-        "api/schema/redoc/",
-        SpectacularRedocView.as_view(url_name="schema"),
-        name="redoc",
-    ),
 ]
